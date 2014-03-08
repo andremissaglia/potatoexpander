6e378f96d071d79beddc2ece086184fe694b502c
André Missaglia <andre.missaglia@gmail.com>
2014-03-08 20:42:24 -0300

    Funcao que inicializa a struct

--- RequestStruct.c
+++ RequestStruct.c
@@ -1,1 +1,7 @@
-
+#include "RequestStruct.h"
+RequestStruct *newRequestStruct(){
+    RequestStruct *req = (RequestStruct *) malloc(sizeof(RequestStruct));
+    req->input = NULL;
+    req->output = NULL;
+    return req;
+}
--- RequestStruct.h
+++ RequestStruct.h
@@ -1,10 +1,12 @@
-#ifndef __COMPRESSREQUEST
-#define __COMPRESSREQUEST
-
+#ifndef __RRQUESTSTRUCT
+#define __REQUESTSTRUCT
+#include <stdlib.h>
+#include <stdio.h>
 typedef struct {
     FILE *input;
     FILE *output;
 } RequestStruct;
 
+RequestStruct *newRequestStruct();
 #endif
 
