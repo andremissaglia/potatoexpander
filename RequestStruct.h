0e30639754273956bd00002a8ae90a714cab00c9
André Missaglia <andre.missaglia@gmail.com>
2014-03-11 13:34:12 -0300

    Adicionado comentários na RequestStruct

--- RequestStruct.h
+++ RequestStruct.h
@@ -2,11 +2,21 @@
 #define __REQUESTSTRUCT
 #include <stdlib.h>
 #include <stdio.h>
+/**
+* Esta estrutura sera passada para o metodo usado, o qual nao precisara lidar com entrada e saida.
+*/
 typedef struct {
     FILE *input;
     FILE *output;
 } RequestStruct;
 
+/**
+* Cria uma nova estrutura.
+*/
 RequestStruct *newRequestStruct();
+
+/**
+* Apaga a estrutura fechando seus arquivos.
+*/
 #endif
 
